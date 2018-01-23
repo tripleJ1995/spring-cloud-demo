dee3cfc52632443f4150db856f7799008a91424e
jijiajian <971791299@qq.com>
2018-01-23 23:10:17 +0800

    init

---
+++ eureka_client_8001/src/main/java/cn/jijiajian/api/DemoApi.java
@@ -0,0 +1,24 @@
+package cn.jijiajian.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author J
+ * @time 2018/1/21 22:30
+ * @description
+ **/
+@RestController
+@RequestMapping(value = "/client1")
+public class DemoApi {
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(String name) {
+        return "hi " + name + ",i am from port:" + port;
+    }
+}
