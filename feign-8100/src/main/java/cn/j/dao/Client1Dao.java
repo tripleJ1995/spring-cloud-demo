1efd534bde423b59fdb5cbc9150dbd6e91b5de15
J <917191299@qq.com>
2018-11-07 23:08:46 +0800

    生版本

--- feign-8100/src/main/java/cn/j/dao/Client1Dao.java
+++ feign-8100/src/main/java/cn/j/dao/Client1Dao.java
@@ -1,7 +1,7 @@
 package cn.j.dao;
 
 import cn.j.dao.hystric.Client1Hystric;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
